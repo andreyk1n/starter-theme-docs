690d642d779e93a3f8f9605fc13fd897ab8e7019
andreyk1n <betas23wr@gmail.com>
2024-12-17 19:37:16 +0200

    update

--- js/functions/scrollHeader.js
+++ js/functions/scrollHeader.js
@@ -3,14 +3,14 @@
 // саме для хедера
 // ---------------------------------------------------------------------------------------------------------------
 export function initScrollControlledHeader() {
-
   let lastScrollTop = 0;
+  const header = document.querySelector('.header');
   window.addEventListener('scroll', () => {
     const scrollTop = window.pageYOffset;
-    document.querySelector('.header').style.top = (scrollTop <= 10 || scrollTop < lastScrollTop) ? '0' : '-100px';
+    header.classList.toggle('scroll-header', scrollTop > 10);
+    header.style.top = (scrollTop <= 10 || scrollTop < lastScrollTop) ? '0' : '-100px';
     lastScrollTop = scrollTop;
   });
-
 }
 // ---------------------------------------------------------------------------------------------------------------
 
